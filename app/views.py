409385a86af9d19827824d6dc846741b392eb05f
Tejachinnu15 <tejachanigalla2000@gmail.com>
2023-07-19 15:28:38 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from app.forms import *
+from django.http import HttpResponse
 # Create your views here.
+def register(request):
+    USFO=UserForm()
+    PFO=ProfileForm()
+    d={'USFO':USFO,'PFO':PFO}
+    if request.method=='POST' and request.FILES:
+         USFD=UserForm(request.POST)
+         PFD=ProfileForm(request.POST,request.FILES)
+         if USFD.is_valid() and PFD.is_valid():
+            NSUFO=USFD.save(commit=False)
+            submitpassword=USFD.cleaned_data['password']
+            NSUFO.set_password(submitpassword)
+            NSUFO.save()
+            NSPO=PFD.save(commit=False)
+            NSPO.username=NSUFO
+            NSPO.save()
+         return HttpResponse('data is submited to database')
+
+    return render(request,'register.html',d)
--- userregisterform/urls.py
+++ userregisterform/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/',register,name='register'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
